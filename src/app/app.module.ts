afa1c5cd4ca2e0255382e3530d606ed30a7d941a
Milos <esoterix202@gmail.com>
2018-12-07 13:45:46 +0100

    Just visuals

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { WhishlistComponent } from './whishlist/whishlist.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    WhishlistComponent
   ],
   imports: [
     BrowserModule,
